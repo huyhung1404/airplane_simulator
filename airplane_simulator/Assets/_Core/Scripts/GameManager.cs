9bcc039ab2cdb862902800ca703f4cad3ff26eb5
huyhung1404 <73883857+huyhung1404@users.noreply.github.com>
2022-05-08 11:03:19 +0700

    8/5

--- airplane_simulator/Assets/_Core/Scripts/GameManager.cs
+++ airplane_simulator/Assets/_Core/Scripts/GameManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Clouds")] [SerializeField] private Transform m_Clouds;
+    [SerializeField] private float m_CloudsHeight;
     private void Awake()
     {
         Application.targetFrameRate = 60;
+        m_Clouds.position = new Vector3(m_Clouds.position.x, m_CloudsHeight, m_Clouds.position.z);
     }
 }
