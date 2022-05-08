907945d43978966dca1b6f4578b0866d0fbc1fa3
huyhung1404 <73883857+huyhung1404@users.noreply.github.com>
2022-05-08 23:58:16 +0700

    Assemble

--- airplane_simulator/Assets/_Core/Scripts/GameManager.cs
+++ airplane_simulator/Assets/_Core/Scripts/GameManager.cs
@@ -1,15 +1,83 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+
+[Serializable]
+public enum TypeSimulator
+{
+    Straight,
+    Back
+}
+
+public enum Role
+{
+    Captain,
+    MemberLeft,
+    MemberRight,
+    MemberBack
+}
 
 public class GameManager : MonoBehaviour
 {
+    public static float CaptainVelocity;
+    private int m_StablePlane = 0;
+    public static GameManager Instance;
+    [Header("Simulator")] [SerializeField] private TypeSimulator m_TypeSimulator;
+    
     [Header("Clouds")] [SerializeField] private Transform m_Clouds;
     [SerializeField] private float m_CloudsHeight;
+
+    [Header("Plane")] 
+    [SerializeField] private float m_TimeWaitPlane;
+    [SerializeField] private SimulatorFlight m_Captain;
+    [SerializeField] private SimulatorFlight m_MemberLeft;
+    [SerializeField] private SimulatorFlight m_MemberRight;
+    [SerializeField] private SimulatorFlight m_MemberBack;
+    [Header("Assemble")] 
+    [SerializeField] private float m_TimeAssemble = 10;
+
+    private Vector3 m_LastCaptainPosition;
+    private Transform m_CaptainTransform;
     private void Awake()
     {
+        Instance = this;
         Application.targetFrameRate = 60;
         m_Clouds.position = new Vector3(m_Clouds.position.x, m_CloudsHeight, m_Clouds.position.z);
+        m_CaptainTransform = m_Captain.transform;
+        m_LastCaptainPosition = m_CaptainTransform.position;
+    }
+
+    private IEnumerator Start()
+    {
+        var delay = new WaitForSeconds(m_TimeWaitPlane);
+        yield return new WaitForSeconds(1);
+        m_Captain.StartSimulator(m_TypeSimulator,Role.Captain);
+        yield return delay;
+        m_MemberLeft.StartSimulator(m_TypeSimulator,Role.MemberLeft);
+        yield return delay;
+        m_MemberRight.StartSimulator(m_TypeSimulator,Role.MemberRight);
+        yield return delay;
+        m_MemberBack.StartSimulator(m_TypeSimulator,Role.MemberBack);
+    }
+
+    private void FixedUpdate()
+    {
+        CaptainVelocity = (m_CaptainTransform.position - m_LastCaptainPosition).magnitude / Time.fixedDeltaTime;
+        m_LastCaptainPosition = m_CaptainTransform.position;
+    }
+
+    public void PlaneStable()
+    {
+        if (++m_StablePlane == 4)
+        {
+            var captainPosition = m_CaptainTransform.position;
+            var assembleX = captainPosition.x + m_TimeAssemble * SimulatorFlight.m_HoldingVelocity.x;
+            m_Captain.Assemble(new Vector3(assembleX,captainPosition.y,captainPosition.z),m_TimeAssemble);
+            m_MemberLeft.Assemble(new Vector3(assembleX - 10,captainPosition.y,captainPosition.z - 10),m_TimeAssemble);
+            m_MemberRight.Assemble(new Vector3(assembleX - 10,captainPosition.y,captainPosition.z + 10),m_TimeAssemble);
+            m_MemberBack.Assemble(new Vector3(assembleX - 20,captainPosition.y,captainPosition.z - 20),m_TimeAssemble);
+        }
     }
 }
--- airplane_simulator/Assets/_Core/Scripts/SimulatorFlight.cs
+++ airplane_simulator/Assets/_Core/Scripts/SimulatorFlight.cs
@@ -8,11 +8,12 @@ public enum State
     GetAltitude,
     Stable,
     Turning,
-    Back
+    Back,
+    None,
+    Assemble
 }
 public class SimulatorFlight : MonoBehaviour
 {
-    [SerializeField] private float Velocity;
     [Header("Graphic")] 
     [SerializeField] private Transform m_Graphic;
     [SerializeField] private AnimationPlane m_Animation;
@@ -34,8 +35,7 @@ public class SimulatorFlight : MonoBehaviour
     [SerializeField] private float SH;
     [SerializeField] private float T3;
     [SerializeField] private AnimationCurve Curve3;
-    [Header("Phase 4")] 
-    [SerializeField] private bool UsePhase4;
+    [Header("Phase 4")]
     [SerializeField] private float m_TimeStartPhase;
     [SerializeField] private float R;
     [SerializeField] private float T4;
@@ -44,20 +44,42 @@ public class SimulatorFlight : MonoBehaviour
     [SerializeField] private AnimationCurve Curve6;
     [SerializeField] private AnimationCurve Curve7;
 
+
+    [SerializeField] private AnimationCurve m_AssemblyCurve;
     private Rigidbody m_Rigidbody;
     private State m_CurrentState;
     private Vector3 m_LastGraphicPosition;
     private Quaternion m_NextGraphicRotation;
-    private Vector3 m_HoldingVelocity;
-    private IEnumerator Start()
+    public static Vector3 m_HoldingVelocity;
+    private TypeSimulator m_TypeSimulator;
+    private Role m_Role;
+    private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
-        m_LastGraphicPosition = m_Graphic.position;
-        m_CurrentState = State.GetAltitude;
-        yield return new WaitForSeconds(1);
-        StartFlight();
+        m_CurrentState = State.None;
     }
 
+    public void StartSimulator(TypeSimulator _type,Role _role)
+    {
+        m_TypeSimulator = _type;
+        m_Role = _role;
+        var plane = transform.parent;
+        var path = new[]
+        {
+            plane.position,
+            new Vector3(plane.position.x,plane.position.y,-10),
+            new Vector3(plane.position.x + 10,plane.position.y,-7.77f),
+        };
+
+        plane.DOPath(path, 10, PathType.CatmullRom).SetEase(Ease.InOutSine).OnComplete(() =>
+        {
+            m_CurrentState = State.GetAltitude;
+            m_LastGraphicPosition = m_Graphic.position;
+            StartFlight();
+        });
+        plane.DORotate(Vector3.zero, 4).SetDelay(4.5f);
+    }
+    
     private void StartFlight()
     {
         m_Rigidbody.DOMoveX(m_Rigidbody.position.x + S0, T0).SetEase(Curve0).OnComplete(() =>
@@ -70,10 +92,18 @@ public class SimulatorFlight : MonoBehaviour
                     m_Rigidbody.DOMove(new Vector3(m_Rigidbody.position.x + SH,m_Rigidbody.position.y + H,m_Rigidbody.position.z), T3).SetEase(Curve3).OnComplete(() =>
                     {
                         m_CurrentState = State.Stable;
-                        m_HoldingVelocity = Velocity * Vector3.right;
-                        if (UsePhase4)
+                        if (m_Role == Role.Captain)
                         {
-                            StartCoroutine(Phase5());
+                            m_HoldingVelocity = GameManager.CaptainVelocity * Vector3.right;
+                        }
+                        switch (m_TypeSimulator)
+                        {
+                            case TypeSimulator.Straight:
+                                GameManager.Instance.PlaneStable();
+                                break;
+                            case TypeSimulator.Back:
+                                StartCoroutine(Phase5());
+                                break;
                         }
                     });
                 });
@@ -85,24 +115,23 @@ public class SimulatorFlight : MonoBehaviour
     {
         switch (m_CurrentState)
         {
+            case State.None:
+                return;
             case State.Stable:
                 m_Rigidbody.velocity = m_HoldingVelocity;
                 break;
             case State.Back:
                 m_Rigidbody.velocity = m_HoldingVelocity;
                 m_Graphic.position = Vector3.Lerp(m_LastGraphicPosition, transform.position, 0.05f);
-                Velocity = (m_Graphic.position - m_LastGraphicPosition).magnitude / Time.fixedDeltaTime;
                 m_LastGraphicPosition = m_Graphic.position;
                 return;
             case State.Turning:
                 m_Graphic.position = Vector3.Lerp(m_LastGraphicPosition, transform.position, 0.05f);
-                Velocity = (m_Graphic.position - m_LastGraphicPosition).magnitude / Time.fixedDeltaTime;
                 m_LastGraphicPosition = m_Graphic.position;
                 return;
         }
         
         m_Graphic.position = Vector3.Lerp(m_LastGraphicPosition, transform.position, 0.05f);
-        Velocity = (m_Graphic.position - m_LastGraphicPosition).magnitude / Time.fixedDeltaTime;
         m_NextGraphicRotation = Quaternion.Euler(Vector3.forward * Vector2.SignedAngle(Vector2.right,m_Graphic.position - m_LastGraphicPosition));
         m_Graphic.rotation = Quaternion.Lerp(m_Graphic.rotation,m_NextGraphicRotation,0.15f);
         m_LastGraphicPosition = m_Graphic.position;
@@ -113,7 +142,6 @@ public class SimulatorFlight : MonoBehaviour
         yield return new WaitForSeconds(m_TimeStartPhase);
         m_CurrentState = State.Turning;
         var currentPosition = transform.position;
-        // m_Rigidbody.velocity = Vector3.zero;
         var path = new[]
         {
             currentPosition,
@@ -124,7 +152,7 @@ public class SimulatorFlight : MonoBehaviour
         m_Rigidbody.DOPath(path, T4, PathType.CatmullRom).SetEase(Curve5).OnComplete(() =>
         {
             m_CurrentState = State.Back;
-            m_HoldingVelocity = Velocity * Vector3.left;
+            m_HoldingVelocity = GameManager.CaptainVelocity * Vector3.left;
         });
         m_Graphic.DORotate(new Vector3(55, -90, 0), T4*0.5f).SetEase(Curve6).OnComplete(() =>
         {
@@ -133,6 +161,14 @@ public class SimulatorFlight : MonoBehaviour
                 m_Graphic.DORotate(new Vector3(0, -180, 0), T4);
             });
         });
-        
+    }
+
+    public void Assemble(Vector3 position,float time)
+    {
+        m_CurrentState = State.Assemble;
+        m_Rigidbody.DOMove(position, time).SetEase(m_AssemblyCurve).OnComplete(() =>
+        {
+            m_CurrentState = State.Stable;
+        });
     }
 }
